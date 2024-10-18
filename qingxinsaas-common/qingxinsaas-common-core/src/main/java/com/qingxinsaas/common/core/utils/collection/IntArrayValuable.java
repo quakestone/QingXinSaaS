7df479890e0e62c13c6a829acac7593758ff4cc3
ywk <2630761383@qq.com>
2024-10-18 15:46:30 +0800

    在MyBatisPlus基础上拓展，提供更的能力

---
+++ qingxinsaas-common/qingxinsaas-common-core/src/main/java/com/qingxinsaas/common/core/utils/collection/IntArrayValuable.java
@@ -0,0 +1,15 @@
+package com.qingxinsaas.common.core.utils.collection;
+
+/**
+ * 可生成 Int 数组的接口
+ *
+ * hasPermi
+ */
+public interface IntArrayValuable {
+
+    /**
+     * @return int 数组
+     */
+    int[] array();
+
+}
